d60d8a0392876e57ca9100571c01e39a7ff9e8a7
Suraj Yadav <suraj.yadav@stirrup.works>
2025-06-21 18:34:51 +0530

    add logo

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -206,7 +206,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <Image
-        src="https://assets.aceternity.com/logo-dark.png"
+        src="/logo.jpg"
         alt="logo"
         width={30}
         height={30}
