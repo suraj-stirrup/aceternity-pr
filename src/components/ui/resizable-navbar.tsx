05ae963b834cf3434502727b0bfa47ea5dc2e56b
Suraj Yadav <suraj.yadav@stirrup.works>
2025-06-21 19:36:01 +0530

    add name

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -211,7 +211,7 @@ export const NavbarLogo = () => {
         width={30}
         height={30}
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+      <span className="font-medium text-black dark:text-white">Stirrup Communications</span>
     </a>
   );
 };
